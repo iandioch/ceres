d146b94b6b8c037370262a355607e933471c99a8
iandioch <iandioch11@gmail.com>
2016-11-23 19:54:11 -0500

    Add cloc functionality to kattis card

--- cards/kattis/run.py
+++ cards/kattis/run.py
@@ -4,6 +4,31 @@ import json
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
+def get_cloc_data():
+	data = {}
+	with open('/tmp/cloc.csv', 'r') as filereader:
+		csv = filereader.read()
+		print(csv)
+		curr_lang = 0
+		total_files = 0
+		langs = {}
+		for lang in csv.split('\n')[1:]:
+			parts = lang.split(',')
+			if len(parts) == 1:
+				break
+			print(parts)
+			langs[parts[1]] = int(parts[0])
+
+		for lang in sorted(langs, key=lambda x:langs[x], reverse=True):
+			data['lang%d' % curr_lang] = lang
+			num_files = langs[lang]
+			data['num_lang%d' % curr_lang] = num_files
+			total_files += num_files
+			curr_lang += 1
+		data['num_solutions'] = total_files
+	return data
+
+
 def number_to_position(n):
 	s = str(n)
 	if s[-1] == '1':
@@ -53,6 +78,9 @@ if __name__ == '__main__':
 	page = load_page('https://open.kattis.com/users/iandioch')
 	data = get_user_data(page)
 	data['irish_rank'] = get_rank_data(load_page('https://open.kattis.com/countries/IRL'))
+	cloc_data = get_cloc_data()
+	for k in cloc_data:
+		data[k] = cloc_data[k]
 	json_data = json.dumps(data, indent=4)
 	with open('/home/waterloo/ceres/cards/kattis/data.json', 'w') as filewriter:
 		filewriter.write(json_data)
---
+++ cards/kattis/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/waterloo/ceres/cards/kattis
+git clone https://github.com/iandioch/solutions.git /tmp/solutions
+cloc --skip-uniqueness --csv --quiet --3 --out=/tmp/cloc.csv /tmp/solutions/kattis
+python3 run.py
+rm -rf /tmp/solutions
+rm /tmp/cloc.csv
