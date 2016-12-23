721240d4f281d3158b44a085a5080a45a9456d6a
iandioch <iandioch11@gmail.com>
2016-12-22 21:28:35 -0500

    Remove cloc progress output from euler script

--- cards/euler/script.sh
+++ cards/euler/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/waterloo/ceres/cards/euler
 git clone https://github.com/iandioch/solutions.git /tmp/solutions
-cloc --skip-uniqueness --csv --quiet --3 --out=/tmp/cloc.csv /tmp/solutions/project_euler
+cloc --skip-uniqueness --csv --quiet --progress-rate=0 --3 --out=/tmp/cloc.csv /tmp/solutions/project_euler
 python2 analyse.py
 rm -rf /tmp/solutions
 rm /tmp/cloc.csv
