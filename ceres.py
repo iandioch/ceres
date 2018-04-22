6a1abfd95e4f4d562d47eae37510d472e5e26a80
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-04-22 17:49:30 +0200

    convert tabs to spaces in ceres.py

--- ceres.py
+++ ceres.py
@@ -5,62 +5,63 @@ import sys
 
 app = Flask(__name__)
 
-env = Environment(loader=FileSystemLoader('/home/waterloo/ceres/templates'))
+env = Environment(loader=FileSystemLoader('/home/noah/prog/ceres/templates'))
 
 outs = {
-	"resume": "/out/cv",
-	"cv": "/raw/cv.pdf",
-	"twitter": "https://twitter.com/iandioch",
-	"linkedin": "https://ie.linkedin.com/in/iandioch",
-	"facebook": "https://facebook.com/noahdonnelly",
-	"github": "https://github.com/iandioch" 
+    "resume": "/out/cv",
+    "cv": "/raw/cv.pdf",
+    "twitter": "https://twitter.com/iandioch",
+    "linkedin": "https://ie.linkedin.com/in/iandioch",
+    "facebook": "https://facebook.com/noahdonnelly",
+    "github": "https://github.com/iandioch" 
 }
 
 def render_template(name, d):
-	template = env.get_template(name)
-	return template.render(d)
+    template = env.get_template(name)
+    return template.render(d)
 
 def create_card(name):
-	return Card(name, "cards/" + name + "/template.html", "cards/" + name + "/data.json")
+    return Card(name, "cards/" + name + "/template.html", "cards/" + name + "/data.json")
 
 cards = {
-	"openbugbounty": create_card("openbugbounty"),
-	"euler": create_card("euler"),
-	"kattis": create_card("kattis"),
-	"lastfm": create_card("lastfm"),
+    "openbugbounty": create_card("openbugbounty"),
+    "euler": create_card("euler"),
+    "kattis": create_card("kattis"),
+    "lastfm": create_card("lastfm"),
 }
 
 @app.route("/card/<string:name>")
 def get_card(name):
-	if name in cards:
-		return cards[name].get_data()
-	return "ERROR"
+    if name in cards:
+        return cards[name].get_data()
+    return "ERROR"
 
 @app.route("/raw/<path:loc>")
 def get_raw(loc):
-	return send_from_directory('raw', loc)
+    return send_from_directory('raw', loc)
 
 @app.route("/out/<path:loc>")
 def get_out(loc):
-	if loc in outs:
-		return '<meta http-equiv="refresh" content="0;url=%s">'%outs[loc]
-	return "<h1>uwu what's this</h1>"
+    if loc in outs:
+        return '<meta http-equiv="refresh" content="0;url=%s">'%outs[loc]
+    return "<h1>uwu what's this</h1>"
 
 @app.route("/")
 def get_index():
-	d = {
-		'cards': [vars(cards[c]) for c in sorted(cards)]
-	}
+    d = {
+        'cards': [vars(cards[c]) for c in sorted(cards)]
+    }
+    print(d)
 
-	print('TEMPLATE, yeah bb')
-	return render_template('index.html', d)
+    print('TEMPLATE, yeah bb')
+    return render_template('index.html', d)
 
 @app.route("/<path:loc>")
 def get_root(loc):
-	print('Requested "%s"' % loc)
-	if loc == '' or loc == '/':
-		return get_index()
-	return get_raw(loc)
+    print('Requested "%s"' % loc)
+    if loc == '' or loc == '/':
+        return get_index()
+    return get_raw(loc)
 
 if __name__ == "__main__":
-	app.run(host="localhost", port=9010)
+    app.run(host="localhost", port=9010)
